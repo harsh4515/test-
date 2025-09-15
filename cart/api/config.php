a9831523c0ef5ac694c3ea894035fee4156e9174
Harsh Khatri <harshkhatri168@gmail.com>
2025-09-15 23:14:41 +0530

    changes

--- cart/api/config.php
+++ cart/api/config.php
@@ -1,11 +1,17 @@
-<!DOCTYPE html>
-<html lang="javascriptreact">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>dffnmv
-    
-</body>
-</html>
+<?php 
+
+$emp =[
+    [1,"Harsh","Salesman",50000],
+    [2,"jay","Salesman",50000],
+    [3,"lalo","salesing",50000],
+    [4,"mami","marketing",40000],
+    [5,"dishu","majoor",6000]
+];
+echo "<table border='1px' cellpadding='5px' cellspacing='0px'>";
+
+    foreach($emp as list($id,$name,$designation,$salary)){
+        echo"<tr><td>$id</td><td>$name</td><td>$designation</td><td>$salary</td></tr>";
+    }
+  echo "</table>";
+  
+?>
--- cart/loop/while.php
+++ cart/loop/while.php
@@ -3,19 +3,15 @@
 $emp =[
     [1,"Harsh","Salesman",50000],
     [2,"jay","Salesman",50000],
-    [3,"lalo","Salesman",50000],
-    [4,"mami","marketing",50000],
-    [5,"dishu","majoor",50000]
+    [3,"lalo","salesing",50000],
+    [4,"mami","marketing",40000],
+    [5,"dishu","majoor",6000]
 ];
+echo "<table border='1px' cellpadding='5px' cellspacing='0px'>";
 
-echo "<table border='1px' cellpadding='5px'>";
-foreach($emp as $v1){
-
-    echo "<tr>";
-    foreach($v1 as $v2){
-echo "<td> $v2</td>";
+    foreach($emp as list($id,$name,$designation,$salary)){
+        echo"<tr><td>$id</td><td>$name</td><td>$designation</td><td>$salary</td></tr>";
     }
-    echo "</tr>";
-}
-echo "</table>";
+  echo "</table>";
 ?>
+s
